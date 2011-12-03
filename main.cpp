90eb18b885c1f3bf169722ef8dd8c14b135827fd
buonopaolo@gmail.com <buonopaolo@gmail.com@67c0cc06-8483-f36d-9645-c2ddfcc419d4>
2011-12-03 15:25:25 +0000

    First code commit

--- main.cpp
+++ main.cpp
@@ -1,26 +1,95 @@
 /***************************************************************************
- *   Copyright (C) 2011 by Paolo Buono                                     *
- *   buono@di.uniba.it                                                     *
+ *   Copyright (C) 2011 by Paolo Buono - IVU Lab.                          *
+ *   http://ivu.di.uniba.it - buono@di.uniba.it                            *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
+ *   This file is part of ASD-dib.                                         *
+ *   ASD-dib is free software; you can redistribute it and/or modify       *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
+ *   ASD-dib is distributed in the hope that it will be useful,            *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  *   GNU General Public License for more details.                          *
  *                                                                         *
  *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *   along with ASD-dib; if not, see <http://www.gnu.org/licenses/>        *
  ***************************************************************************/
- 
- 
-// code goes here
- 
-// code goes here too
- 
- 
+
+#include <cstdlib>
+#include <iostream>
+
+#include "Dizionario/datoDizionario.h"
+#include "Dati/testo.h"
+#include "Dati/persone.h"
+
+#include "Lista/lista.h"
+#include "Coda/coda.h"
+#include "Albero/alberobin.h"
+#include "Dizionario/dizionario.h"
+#include "Dizionario/dizionarioLista.h"
+#include "Dizionario/dizionarioVettore.h"
+#include "Dizionario/dizionarioHash.h"
+#include "Dizionario/dizionarioAlberoBinario.h"
+#include "Dati/libreria.h"
+
+#include "Servizio/servizioAlberobin.h"
+#include "Servizio/servizioTemplate.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    //    cout << "Creo lista di prestiti" << endl;
+        Lista<Lista<Prestiti> > libreria;
+    //    CaricaLibreria(libreria);
+    
+    cout << "Carico e stampo elenco libri" << endl;
+    Lista <Testo> l;
+    CaricaLista(l);
+    Messaggio(l);
+    stampaLista(l);
+    
+    cout << "Carico e stampo le persone" << endl;
+    Lista<Persona> p;
+    CaricaLista(p);
+    Messaggio(p);
+    stampaLista(p);
+    
+    cout << "Carico e stampo altri dati: " << endl;
+    Coda<Persona> c;
+    caricaCoda(c);
+    stampaCoda(c);
+    
+    DizionarioVettore dv;
+    caricaDizionario(dv);
+    dv.cancella(1);
+    elemento ev=dv.cerca(2);
+    if(ev!="") cout << "Trovato elemento: " << ev << endl;
+    else cout << "Elemento non trovato"<<endl;
+    
+    DizionarioLista dl;
+    caricaDizionario(dl);
+    dv.cancella(1);
+    elemento el=dl.cerca(2);
+    if(el!="") cout << "Trovato elemento dizionario lista: " << el << endl;
+    else cout << "Elemento non trovato in dizionario lista"<<endl;
+    
+    DizionarioAlberoBinario db;
+    caricaDizionario(db);
+    dv.cancella(1);
+    elemento eb=db.cerca(2);
+    if(eb!="") cout << "Trovato elemento dizionario alberobin: " << eb << endl;
+    else cout << "Elemento non trovato in dizionario alberobin"<<endl;
+    
+    DizionarioHash dh;
+    caricaDizionario(dh);
+    dh.cancella(1);
+    elemento eh=dh.cerca(2);
+    if(eh!="") cout << "Trovato elemento dizionario hash: " << eh << endl;
+    else cout << "Elemento non trovato in dizionario hash"<<endl;
+
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
