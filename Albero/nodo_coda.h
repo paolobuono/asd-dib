d04c8d46dc9be50d4ff653947094ed02de999fe4
asterisko90@virgilio.it <asterisko90@virgilio.it@67c0cc06-8483-f36d-9645-c2ddfcc419d4>
2011-12-08 00:26:19 +0000

    aggiunto nodo della coda

---
+++ Albero/nodo_coda.h
@@ -0,0 +1,51 @@
+#ifndef NODO_CODA_H
+#define NODO_CODA_H
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+
+
+template <class tipoelem>
+class nodocoda {
+      private:
+       tipoelem elem;
+       nodocoda<tipoelem>* succ;
+      public:
+             nodocoda();
+             ~nodocoda();
+             tipoelem leggiElem();
+             void scriviElem(tipoelem);
+             nodocoda<tipoelem>* leggiSucc();
+             void scriviSucc(nodocoda<tipoelem>*);
+             
+};
+
+template<class tipoelem>
+nodocoda<tipoelem>::nodocoda() {
+                     
+                     } 
+template<class tipoelem>                                 
+nodocoda<tipoelem>::~nodocoda() {
+                      
+                      }
+template<class tipoelem>              
+tipoelem nodocoda<tipoelem>::leggiElem() {
+    return elem;
+    }
+    
+template<class tipoelem>    
+void nodocoda<tipoelem>::scriviElem(tipoelem e) {
+     elem=e;
+     }
+
+template<class tipoelem>
+nodocoda<tipoelem>* nodocoda<tipoelem>::leggiSucc() {
+     return succ;
+     }
+     
+template<class tipoelem>
+void nodocoda<tipoelem>::scriviSucc(nodocoda<tipoelem>* p) {
+     succ=p;
+     }
+#endif
