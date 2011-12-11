c7bdc397d9fa6963fadb425b043ae59943e6ead0
SASSOPATTY@gmail.com <SASSOPATTY@gmail.com@67c0cc06-8483-f36d-9645-c2ddfcc419d4>
2011-12-11 10:35:12 +0000

    Aggiunta del nodo Lista

---
+++ Lista/cella.cpp
@@ -0,0 +1,17 @@
+#include "cella.h"
+
+
+cella:: cella()
+{   
+    }
+    
+tipoelem cella:: leggi_valore()
+{
+         return valore;
+         } 
+         
+void cella:: scrivi_valore(tipoelem v)
+{
+     valore=v;
+     }   
+              
