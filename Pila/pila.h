fdb5cc94f74907e2943e7e29cdf2f9b6cdd8504d
SASSOPATTY@gmail.com <SASSOPATTY@gmail.com@67c0cc06-8483-f36d-9645-c2ddfcc419d4>
2011-12-11 10:46:44 +0000

    creazione della Pila con il vettore

---
+++ Pila/pila.h
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <iostream>
+
+#define MAX_LUNG 4 
+using namespace std;
+
+typedef int tipoelem;
+
+class pila{
+           private:
+                   tipoelem elementi[MAX_LUNG];
+                   int testa;
+                   
+            public:
+                   pila();
+                   void creapila();
+                   bool pilavuota();
+                   tipoelem leggipila();
+                   void fuoripila();
+                   void inpila(tipoelem);
+                   
+};
