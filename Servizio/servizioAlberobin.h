16d2c2a76bc29a381585f553818e10de58e323e4
SASSOPATTY@gmail.com <SASSOPATTY@gmail.com@67c0cc06-8483-f36d-9645-c2ddfcc419d4>
2011-12-15 20:55:37 +0000

    ordinamento e ricerca binaria

--- Servizio/servizioAlberobin.h
+++ Servizio/servizioAlberobin.h
@@ -35,6 +35,13 @@ void inserimento( Dato<string> ,Alberobin< Dato<string> > &);  // inserimento  c
 int maxprofondita(CellaAlbero < Dato<string> > *,Alberobin< Dato<string> >);
 int profnodo(CellaAlbero < Dato<string> > *,Alberobin< Dato<string> >,int, Dato<string> );
 
+Coda<T> costruisci_coda(Coda<T> &, Albero_bin<T> &, Nodo<T>*);
+void ordina_albero(Coda<T> &, Albero_bin<T> &,Albero_bin<T> &, Nodo<T>*, bool);
+void inserimento_2(T,Nodo<T>*, Albero_bin<T> &);
+
+Nodo<T>* ricerca_binaria(Albero_bin<T> &, int, Nodo<T>*);
+
+
 //Implementazione
 // visita in preordine
 void previsita (CellaAlbero < Dato<string> > * u, Alberobin< Dato<string> > T){
@@ -206,5 +213,85 @@ int profnodo (CellaAlbero < Dato<string> > * u,Alberobin< Dato<string> > T,int l
     }
 	return liv;
 }
+
+
+
+
+//le seguenti procedure "costruisci_coda", "ordina_albero" e "inserimento_2" servono per ordinare un albero binario che non sia stato caricato con
+//l'"inserimentOrdinato"
+
+
+Coda<T> costruisci_coda(Coda<T> & c, Albero_bin<T> &t, Nodo<T>* pos_1){             //inserisce gli elementi dell'albero da ordinare in una coda     
+   if(!t.bin_albero_vuoto()){
+        if(!t.sinistro_vuoto(pos_1)){        
+            costruisci_coda(c, t, t.figlio_sinistro(pos_1));                         
+                                    }                        
+        c.incoda(pos_1);                                              
+        if(!t.destro_vuoto(pos_1)){                               
+           costruisci_coda(c, t, t.figlio_destro(pos_1));                   
+                                  }                   
+                            }
+                        return c;                                   
+}
+
+
+void ordina_albero(Coda<T> &c, Albero_bin<T> & t, Albero_bin<T> & a, Nodo<T>* pos_1){            //legge gli elementi della coda e per ciascuno di essi                 c=costruisci_coda(c, t, pos_1);                                                                  //chiama la funzione di inserimento
+  T e;
+
+while(!c.codavuota()){
+        e=c.leggicoda()->leggi_info();
+        inserimento_2(e, a.bin_radice(), a);
+        c.fuoricoda();             
+                      }        
+}
+
+
+void inserimento_2(T e, Nodo<T>* p, Albero_bin<T> &a){                          //inserisce gli elementi nell'albero rispettando la relazione d'ordine totale
+  if(a.bin_albero_vuoto()){
+       a.ins_bin_radice(e);                     
+                          }     
+  else{            
+       if(e<a.leggi_nodo(p)){
+           if (a.sinistro_vuoto(p)){
+               a.ins_figlio_sinistro(e,p);
+                                   }       
+           else{
+               inserimento_2(e,a.figlio_sinistro(p),a);
+               }
+                            }                                                 
+                                                     
+      if(e>= a.leggi_nodo(p)){ 
+          if(a.destro_vuoto(p)){ 
+              a.ins_figlio_destro(e,p);
+                                }                                         
+          else{ 
+              inserimento_2(e,a.figlio_destro(p),a); 
+              }                             
+                             }                                                    
+     }
+
+}
+
+
+Nodo<T>* ricerca_binaria(Albero_bin<T> & t, int k, Nodo<T>* p1){      //suppondendo una chiave di ricerca di tipo intero (k), effettua la ricerca 
+   Nodo<T>* j=NULL;                                                   //dell'elemento all'interno dell'albero restituendone la posizione
+ 
+    if (!t.bin_albero_vuoto()){
+              if(k == t.leggi_nodo(p1)){  
+                    j=p1; 
+                                       }
+              else
+                  if(k < t.leggi_nodo(p1)){ 
+                        if(!t.sinistro_vuoto(p1))
+                            j=ricerca_binaria(t, k, t.figlio_sinistro(p1));
+                                          }
+                  else 
+                     if(!t.destro_vuoto(p1))    
+                           j=ricerca_binaria(t, k, t.figlio_destro(p1));   
+            
+                              }   
+    return j;   
+};
+
 
 #endif
