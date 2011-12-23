2e8fa84d11210389e853d2d8d3f1dc52beff405f
asterisko90@virgilio.it <asterisko90@virgilio.it@67c0cc06-8483-f36d-9645-c2ddfcc419d4>
2011-12-23 15:44:09 +0000

    -aggiunte varie info sull'altezza dell'albero e l'utilizzo della variabile temp

--- Servizio/servizi_e-ario.h
+++ Servizio/servizi_e-ario.h
@@ -176,6 +176,7 @@ int conta_foglie(Albero_enn<studente> & t, Nodo<studente>* u, int cont)     //re
 };
 
 
+/*Perchè utilizzo di temp? l'altezza dell'albero è il cammino piu lungo dalla radice al nodo foglia quindi si dovrebbe passare la radice */
 
 int altezza(albero<sudente> & t, Nodo u,int temp)  //restituisce l'altezza dell'albero
 {
