5f83a590bbb55e2f241707069779be5fffc2a2f2
monaco.d@gmail.com <monaco.d@gmail.com@67c0cc06-8483-f36d-9645-c2ddfcc419d4>
2012-01-13 17:45:27 +0000

    Niente di trascendentale solo piccoli fix

--- Grafo/grafoListeAdiacenza.h
+++ Grafo/grafoListeAdiacenza.h
@@ -21,6 +21,7 @@
 #define _GrafoListeAdiacenza_H
 
 #include <stdlib.h>
+#include <cstddef>
 
 using namespace std;
 typedef bool boolean;
@@ -28,64 +29,76 @@ typedef bool boolean;
 #include "grafo.h"          //Classe virtuale di Grafo
 #include "nodoGrafo.h"      //Classe del nodo grafo,  necessita di allegare un "tipoNodo"
 #include "arcoGrafo.h"      //Classe dell'arco grafo, necessita di allegare un "tipo arco"
-
 #include "../Lista/listaUnidirezionale.h" //Utile per creare la lista di nodi del grafo
-
-template <class tipoNodo, class tipoArco>
-class GrafoListeAdiacenza : public Grafo< tipoNodo, tipoArco > {
+template<class tipoNodo, class tipoArco>
+class GrafoListeAdiacenza: public Grafo<tipoNodo, tipoArco> {
 private:
-	ListaUnidirezionale <ListaUnidirezionale < nodoGrafo < tipoNodo > > > listaAdiacenza;
+	ListaUnidirezionale<ListaUnidirezionale<nodoGrafo<tipoNodo> > > listaAdiacenza;
+
 public:
+	typedef nodoGrafo<tipoNodo> nodoGrafoListaAdiacenza;
+
 	void creaGrafo();
 	boolean grafoVuoto();
-	void setOrientato( boolean setOrientato );
-	void setPesato( boolean setPesato );
+	void setOrientato(boolean setOrientato);
+	void setPesato(boolean setPesato);
 
-	void insNodo(nodoGrafo< tipoNodo >, tipoNodo);
-	void cancNodo(nodoGrafo< tipoNodo >);
-	boolean esisteNodo(nodoGrafo< tipoNodo >);
-	tipoNodo leggiNodo(nodoGrafo< tipoNodo >);
+	void insNodo(nodoGrafo<tipoNodo>, tipoNodo);
+	void cancNodo(nodoGrafo<tipoNodo>);
+	boolean esisteNodo(nodoGrafo<tipoNodo>);
+	tipoNodo leggiNodo(nodoGrafo<tipoNodo>);
 
-	void insArco(nodoGrafo< tipoNodo >, nodoGrafo< tipoNodo >);
-	void insArco(nodoGrafo< tipoNodo >, nodoGrafo< tipoNodo >, arcoGrafo < tipoArco >);
-	void cancArco(nodoGrafo< tipoNodo >, nodoGrafo< tipoNodo >);
-	boolean esisteArco(nodoGrafo< tipoNodo >, nodoGrafo< tipoNodo >);
-	arcoGrafo < tipoArco > leggiArco(nodoGrafo< tipoNodo >, nodoGrafo< tipoArco >);
+	void insArco(nodoGrafo<tipoNodo>, nodoGrafo<tipoNodo>);
+	void insArco(nodoGrafo<tipoNodo>, nodoGrafo<tipoNodo>, arcoGrafo<tipoArco>);
+	void cancArco(nodoGrafo<tipoNodo>, nodoGrafo<tipoNodo>);
+	boolean esisteArco(nodoGrafo<tipoNodo>, nodoGrafo<tipoNodo>);
+	arcoGrafo<tipoArco> leggiArco(nodoGrafo<tipoNodo>, nodoGrafo<tipoArco>);
 
-	ListaUnidirezionale< Nodo< tipoNodo > > *adiacenti(nodoGrafo< tipoNodo >);
+	//Funzioni Aggiuntive
+	nodoGrafo<tipoNodo> creaNodo();
+
+	ListaUnidirezionale<Nodo<tipoNodo> > *adiacenti(nodoGrafo<tipoNodo>);
 };
 
-
-template< class tipoNodo, class tipoArco >
-void GrafoListeAdiacenza< tipoNodo, tipoArco >::creaGrafo(){
+template<class tipoNodo, class tipoArco>
+void GrafoListeAdiacenza<tipoNodo, tipoArco>::creaGrafo() {
 
 	//inizializza il grafo con la lista di adiacenza vuota
-	listaAdiacenza.crealista();
+	listaAdiacenza.creaLista();
+}
+
+template<class tipoNodo, class tipoArco>
+nodoGrafo<tipoNodo> GrafoListeAdiacenza<tipoNodo, tipoArco>::creaNodo() {
+
+	nodoGrafo<tipoNodo> nodo;
+	return (nodo);
 }
 
 /*
-INSNODO (u, a, G) = G'
-PRE: G = (N,A) u NON APPARTIENE ad N, a E’ DI TIPO TIPONODO
-POST: G' = (N’,A) N' = N U {u}
+ INSNODO (u, a, G) = G'
+ PRE: G = (N,A) u NON APPARTIENE ad N, a E’ DI TIPO TIPONODO
+ POST: G' = (N’,A) N' = N U {u}
  */
-template < class tipoNodo, class tipoArco >
-void GrafoListeAdiacenza< tipoNodo, tipoArco >::insNodo(nodoGrafo< tipoNodo > nodo, tipoNodo infNodo){
+template<class tipoNodo, class tipoArco>
+void GrafoListeAdiacenza<tipoNodo, tipoArco>::insNodo(nodoGrafo<tipoNodo> nodo,
+		tipoNodo infNodo) {
 	//creo un nuovo nodo
 	nodoGrafo<tipoNodo> nuovo_nodo;
 
 	//allego l'informazione
-	nuovo_nodo->infoNodo = infNodo;
+	//nuovo_nodo->infoNodo = infNodo;
 
 	//TODO serve un metodo per inserire alla fine della lista
 	//listaAdiacenza.insLista(nuovo_nodo, posizione&);
 }
 /*
-INSARCO (u, v, peso G) = G‘
-PRE: u APPARTIENE ad N, v APPARTIENE ad N e non esiste arco tra u e v
-POST: G' = (N,A') , A' = A ? {(u,v)}
+ INSARCO (u, v, peso G) = G‘
+ PRE: u APPARTIENE ad N, v APPARTIENE ad N e non esiste arco tra u e v
+ POST: G' = (N,A') , A' = A ? {(u,v)}
  */
-template < class tipoNodo, class tipoArco >
-void GrafoListeAdiacenza< tipoNodo, tipoArco >::insArco(nodoGrafo< tipoNodo >, nodoGrafo< tipoNodo >){
+template<class tipoNodo, class tipoArco>
+void GrafoListeAdiacenza<tipoNodo, tipoArco>::insArco(nodoGrafo<tipoNodo>,
+		nodoGrafo<tipoNodo>) {
 
 	//SEARCH nodo1 into listaAdiacenza
 	//SEARCH nodo2 into listaAdiecenza
@@ -94,42 +107,43 @@ void GrafoListeAdiacenza< tipoNodo, tipoArco >::insArco(nodoGrafo< tipoNodo >, n
 	//aggiungi nodo adiacente a listaAdiacenza[NODO2]
 }
 
-template < class tipoNodo, class tipoArco >
-boolean GrafoListeAdiacenza< tipoNodo, tipoArco >::esisteArco(nodoGrafo< tipoNodo >, nodoGrafo< tipoNodo >){
+template<class tipoNodo, class tipoArco>
+boolean GrafoListeAdiacenza<tipoNodo, tipoArco>::esisteArco(nodoGrafo<tipoNodo>,
+		nodoGrafo<tipoNodo>) {
 	boolean out;
 
-	return(out);
+	return (out);
 }
 
 /*
-GRAFOVUOTO (G) = b
-PRE: NESSUNA
-POST: b=VERO SE N=0 E A=0 b=FALSO ALTRIMENTI
+ GRAFOVUOTO (G) = b
+ PRE: NESSUNA
+ POST: b=VERO SE N=0 E A=0 b=FALSO ALTRIMENTI
 
-//Modificata, basta controllare se non ci sono NODI in N
-POST: b=VERO SE N=0 b=FALSO ALTRIMENTI
+ //Modificata, basta controllare se non ci sono NODI in N
+ POST: b=VERO SE N=0 b=FALSO ALTRIMENTI
  */
-template < class tipoNodo, class tipoArco >
-boolean GrafoListeAdiacenza< tipoNodo, tipoArco >::grafoVuoto(){
+template<class tipoNodo, class tipoArco>
+boolean GrafoListeAdiacenza<tipoNodo, tipoArco>::grafoVuoto() {
 	boolean out;
-	if(listaAdiacenza.listaVuota()){
+	if (listaAdiacenza.listaVuota()) {
 		out = true;
-	}else{
+	} else {
 		out = false;
 	}
-	return(out);
+	return (out);
 }
 
-
-template < class tipoNodo, class tipoArco >
-boolean GrafoListeAdiacenza< tipoNodo, tipoArco >::esisteNodo(nodoGrafo< tipoNodo > n){
+template<class tipoNodo, class tipoArco>
+boolean GrafoListeAdiacenza<tipoNodo, tipoArco>::esisteNodo(
+		nodoGrafo<tipoNodo> n) {
 	boolean out;
-	if(listaAdiacenza.cerca(n) != null){
-		out=true;
-	}else{
-		out=false;
-	}
-	return(out);
+	/*if (listaAdiacenza.cerca(n)) {
+		out = true;
+	} else {
+		out = false;
+	}*/
+	return (out);
 }
 
 #endif /* _GrafoListeAdiacenza_H */
---
+++ Grafo/servizio_grafoListeAdiacenza.h
@@ -0,0 +1,27 @@
+/*
+ * servizo_grafoListeAdiacenza.h
+ *
+ *  Created on: 11/gen/2012
+ *      Author: kiuzhack
+ */
+
+#ifndef SERVIZO_GRAFOLISTEADIACENZA_H_
+#define SERVIZO_GRAFOLISTEADIACENZA_H_
+
+#include "grafoListeAdiacenza.h"
+
+void servizio_grafoListeAdiacenza();
+
+void servizio_grafoListeAdiacenza() {
+
+	GrafoListeAdiacenza<int, int> grafo;
+	nodoGrafo<int> nodo;
+	nodo = grafo.creaNodo();
+
+	grafo.insNodo(nodo, 20);
+
+    cout << "fine" << endl;
+
+}
+
+#endif /* SERVIZO_GRAFOLISTEADIACENZA_H_ */
