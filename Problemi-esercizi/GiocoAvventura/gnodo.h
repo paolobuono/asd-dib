f7814850b3993ba79cb8e85e67fa8b6c2b416ebb
mancatony@gmail.com <mancatony@gmail.com@67c0cc06-8483-f36d-9645-c2ddfcc419d4>
2012-01-21 10:16:02 +0000

    Semplificazione del codice.

--- Problemi-esercizi/GiocoAvventura/gnodo.h
+++ Problemi-esercizi/GiocoAvventura/gnodo.h
@@ -9,12 +9,9 @@ template <class T, class P>
 class GNodo
 {
 	public:
-		GNodo();
 		GNodo(T);
 		GNodo(T, P);
-		void setNodo(T);
 		T getNodo();
-		void setArco(P);
 		P getArco();
 	private:
 		T info;
@@ -22,33 +19,19 @@ class GNodo
 };
 
 template <class T, class P>
-GNodo<T, P>::GNodo()
-{}
-
-template <class T, class P>
 GNodo<T, P>::GNodo(T elem)
-{
-	info = elem;
-}
+{ this->info = info; }
 
 template <class T, class P>
-GNodo<T, P>::GNodo(T elem, P load)
+GNodo<T, P>::GNodo(T info, P peso)
 {
-	info = elem;
-	peso = load;
+	this->info = info;
+	this->peso = peso;
 }
-
-template <class T, class P>
-void GNodo<T, P>::setNodo(T elem)
-{ info = elem; }
 
 template <class T, class P>
 T GNodo<T, P>::getNodo()
 { return info; }
-
-template <class T, class P>
-void GNodo<T, P>::setArco(P load)
-{ peso = load; }
 
 template <class T, class P>
 P GNodo<T, P>::getArco()
